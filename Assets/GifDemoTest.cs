95a71290efc32165d7c8320be44da03166d6f368
denrei <thedenrei@gmail.com>
2015-03-16 16:37:54 -0400

    Adding Gif Textures and Grapher

---
+++ Assets/GifDemoTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.IO;
+using Gif2Textures;
+using System.Collections;
+
+public class GifDemoTest : MonoBehaviour
+{
+	public string url = "http://piskel-imgstore-b.appspot.com/img/90ae3d68-cb82-11e4-8653-058e997efba9.gif";
+	public bool m_FasterButMoreMemoryUsage = true;
+	
+	float m_Timer = 0.0f;
+	float m_CurrentDelay = 0;
+	GifFrames m_GifFrames = null;
+
+
+	IEnumerator Start() {
+		WWW www = new WWW(url);
+		yield return www;
+
+		Debug.Log(www.text);
+
+		MemoryStream ms = new MemoryStream(www.bytes);
+		
+		m_GifFrames = new GifFrames();
+		if (m_GifFrames.Load(ms, m_FasterButMoreMemoryUsage))
+			SetNextFrame();
+		else
+			m_GifFrames = null;
+	}
+	
+	void Update()
+	{
+		if (m_GifFrames != null)
+		{
+			m_Timer += Time.deltaTime;
+			
+			if (m_Timer >= m_CurrentDelay)
+			{
+				m_Timer -= m_CurrentDelay;
+				SetNextFrame();
+			}
+		}
+	}
+	
+	void SetNextFrame()
+	{
+		Texture2D tex;
+		m_GifFrames.GetNextFrame(out tex, out m_CurrentDelay);
+		if (GetComponent<Renderer>() != null && GetComponent<Renderer>().material != null)
+			GetComponent<Renderer>().material.mainTexture = tex;
+		if (GetComponent<GUITexture>() != null)
+			GetComponent<GUITexture>().texture = tex;
+	}
+}
---
+++ Assets/Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	class Grid {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	Transform CreateWorldBlock(Vector3 pos) {
+		Debug.Log(pos);
+		var t = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+		t.position = new Vector3(pos.x, pos.y, 0);
+		return t;
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0)) {
+			var mouseWorldRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.Log(mouseWorldRay);
+			
+			RaycastHit2D hit = Physics2D.Raycast(mouseWorldRay.origin, mouseWorldRay.direction);
+			if(hit) {
+				Destroy (hit.collider.gameObject);
+			} else {;
+				CreateWorldBlock(mouseWorldRay.origin);
+			}
+		}
+	}
+}
